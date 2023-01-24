df333c736510a1fb70031ab9e7f67abc2da5d1c9
Bryce Reading <66621652+BryceReading@users.noreply.github.com>
2023-01-24 19:53:36 +0000

    Changes to the tablet script and fixed camera script
    Fixed camera script now has a raycast designed to keep the camera a fixed distance from the object.

--- Aircraft Maintenance/Assets/Scripts/Features/Fixed Cam Features/CameraChange.cs
+++ Aircraft Maintenance/Assets/Scripts/Features/Fixed Cam Features/CameraChange.cs
@@ -4,30 +4,11 @@ using UnityEngine;
 
 public class CameraChange : MonoBehaviour
 {
-    //[Header("Camera Mouse Movment")]
-    //public float hSpeed = 2.0f;
-    //public float VSpeed = 2.0f;
-    //private float yaw = 0.0f;
-    //private float pitch = 0.0f;
-
-    //public Transform target;
-    //public Transform camTransform = null;
-    //private Vector3 camOffset = Vector3.zero;
-    //public float speedTurn = 3;
-
-    //GameObject parent;
-
     [HeaderAttribute("Camera keyboard Movment")]
     public float speed;
 
     void Start()
     {
-        //parent = new GameObject();
-        //camTransform = Camera.main.transform;
-        
-        //parent.transform.position = target.position;
-        //camTransform.parent = parent.transform;
-        //camOffset = camTransform.position - target.position;
     }
 
     void Update()
@@ -46,18 +27,16 @@ public class CameraChange : MonoBehaviour
         if (Input.GetKey(KeyCode.DownArrow)) transform.Rotate(Vector3.right, -speed * Time.deltaTime);
         if (Input.GetKeyDown("[0]")) transform.rotation = Quaternion.Euler(0, 0, 0);
 
-        //Maintain a set distance from the object
-        //transform.position = transform.parent.position - transform.forward * 10;
+        //Maintain a set distance from the object usign raycasting
+        RaycastHit hit;
+        
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, 10))
+        {
+            transform.position = hit.point;
+        }
 
         // Zomming in and out
-        
 
-        /// 
-        /// Rotate the camera using the mouse
-        /// 
-        /* yaw += hSpeed * Input.GetAxis("Mouse X");
-         pitch -= VSpeed * Input.GetAxis("Mouse Y");
 
-         parent.transform.eulerAngles = new Vector3(pitch, yaw, 0.0f);*/
     }
 }
