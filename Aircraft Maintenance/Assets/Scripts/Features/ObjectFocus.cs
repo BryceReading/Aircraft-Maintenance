226d8ed63a674ade1d5a9623d8c54f4639d7b34a
Bryce Reading <66621652+BryceReading@users.noreply.github.com>
2023-01-27 15:46:16 +0000

    once focused, object moves towards the camera
    Once the component is clicked and the item is focuses the component moves closer to the player for a better view.

--- Aircraft Maintenance/Assets/Scripts/Features/ObjectFocus.cs
+++ Aircraft Maintenance/Assets/Scripts/Features/ObjectFocus.cs
@@ -9,11 +9,14 @@ public class ObjectFocus : MonoBehaviour
     Transform target;
     bool clicked;
     List<GameObject> objects = new List<GameObject>();
+    Vector3 startPos;
 
     // Start is called before the first frame update
     void Start()
     {
         cam = Camera.main;
+
+        startPos = transform.position;
     }
 
     // Update is called once per frame
@@ -24,13 +27,21 @@ public class ObjectFocus : MonoBehaviour
             // focused the camera on the objecy being clicked
             cam.transform.LookAt(target);
 
+
+            // bring the object close to the player relevent to the distance from the player
+            target.position = Vector3.MoveTowards(target.position, cam.transform.position, 10f);
+            
             clicked = false;
         }
 
         // reset the cam to the original position
         if (Input.GetMouseButtonUp(1))
         {
+            // reset the camera to the original position
             cam.transform.rotation = Quaternion.identity;
+
+            // resets the position of the object to the original position
+            target.position = startPos;
         }
     }
 
